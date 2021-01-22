5e3bee94e8ce7d352d8063449c5f89619035734f
Percy Nizamov <Mysticgummibear@gmail.com>
2021-01-22 06:50:36 -0700

    p1 done

---
+++ Class_2.java
@@ -0,0 +1,35 @@
+//Inheritance: Exercise #1 Class_2
+public class Class_2 extends Class_1 {
+   
+    private int z;
+
+   //x = 0, y = 0, z = 0
+   public Class_2() {
+    x = 0;
+    y = 0;
+    z = 0;
+   }
+
+   //x = x1, y = y1, z = z1
+   public Class_2(int x1, int y1, int z1) {
+        x = x1;
+        y = y1;
+        z = z1;
+   }
+
+   //output x, y, z
+   public void print() {
+        System.out.print(x + " " + y + " " + z + ""); 
+   }
+
+   public String toString() {
+        return x + " " + y + " " + z + " ";
+   }
+
+   //x = x1, y = y1, z = z1
+   public void set(int x1, int y1, int z1) {
+        x = x1;
+        y = y1;
+        z = z1;
+   }
+}
